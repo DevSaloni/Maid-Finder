37717b638b905f28f1775fabca4cac6ccb25f925
Joshna Waikar <joshnawaikar@gmail.com>
2025-05-10 09:32:06 +0000

    applynow card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Herosection from "../component/Herosection";
 import Serviceprovidersection from "../component/Serviceprovidersection";
 import Navbar from '../component/Navbar';
-
+import ApplyNow from '../component/ApplyNow';
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Herosection/>
     <Serviceprovidersection/>
+    <ApplyNow/>
     </>
   )
 }
